236d358e35325b1c18a51018f0993582b068b215
wingfish@gmail.com <bos19731975>
2013-09-06 18:21:47 +0800

    2013.9.6. add listbox to demo add items and clear items

--- uTableview1.pas
+++ uTableview1.pas
@@ -5,7 +5,12 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Dialogs, System.Actions, FMX.ActnList,
-  FMX.TabControl, FMX.ListBox, FMX.Layouts, FMX.StdCtrls;
+  FMX.TabControl, FMX.ListBox, FMX.Layouts, FMX.StdCtrls, FMX.ListView.Types,
+  FMX.ListView, Data.Bind.GenData, Fmx.Bind.GenData, System.Rtti,
+  System.Bindings.Outputs, Fmx.Bind.Editors, Data.Bind.EngExt,
+  Fmx.Bind.DBEngExt, Data.Bind.Components, Data.Bind.ObjectScope, FMX.Edit,
+  FMX.TreeView, FMX.Memo, FMXTee.Engine, FMXTee.Procs, FMXTee.Chart,
+  FMXTee.Chart3D, FMX.Objects3D, FMX.Controls3D;
 
 type
   TForm1 = class(TForm)
@@ -27,18 +32,27 @@ type
     TabControl2: TTabControl;
     TabItem5: TTabItem;
     TabItem6: TTabItem;
-    ActionList1: TActionList;
-    ChangeTabAction1: TChangeTabAction;
     ListBoxGroupHeader1: TListBoxGroupHeader;
     ToolBar1: TToolBar;
     SpeedButton1: TSpeedButton;
-    ChangeTabAction2: TChangeTabAction;
-    CalloutPanel1: TCalloutPanel;
-    Expander1: TExpander;
     Switch1: TSwitch;
     ProgressBar1: TProgressBar;
     Button1: TButton;
+    ListBox3: TListBox;
+    ListBoxHeader1: TListBoxHeader;
+    SearchBox1: TSearchBox;
+    Button2: TButton;
+    SpeedButton2: TSpeedButton;
+    ListView1: TListView;
+    Cube1: TCube;
+    Plane1: TPlane;
+    Chart3DChart1: TChart;
+    Chart3D1: TChart3D;
+    Chart3DChart2: TChart;
+    Chart3D2: TChart3D;
     procedure ListBoxItem1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure actAddItemExecute(Sender: TObject);
   private
     { Private declarations }
   public
@@ -51,6 +65,18 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TForm1.actAddItemExecute(Sender: TObject);
+begin
+  listbox3.Items.Add('test '+FormatDatetime('hh:mm:ss',now));
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+begin
+  listbox3.Items.Clear;
+  listbox3.Items.Add('1');
+  listbox3.Items.Add('2');
+end;
 
 procedure TForm1.ListBoxItem1Click(Sender: TObject);
 begin
